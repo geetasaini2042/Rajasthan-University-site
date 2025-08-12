e4eae7275c9e20a6513c6ac91fb748c6c22da8b2
geetasaini2042 <Geetasaini2042@gmail.com>
2025-08-12 08:45:37 +0530

    Update custom_taxonomies.rb

--- _plugins/custom_taxonomies.rb
+++ _plugins/custom_taxonomies.rb
@@ -1,9 +1,3 @@
-# _plugins/subjects_archive.rb
-#
-# Custom taxonomy archive for "subjects"
-# Handles URLs like /subjects/<subject>/ using _layouts/subject.html
-# Case-insensitive on 'subject' vs 'subjects'
-
 module Jekyll
   class SubjectsPage < Page
     def initialize(site, base, dir, subject)
@@ -26,9 +20,7 @@ module Jekyll
     def generate(site)
       subjects = site.posts.docs.flat_map { |post| post.data['subjects'] || [] }
       subjects.uniq.each do |subject|
-        # Normalize folder to lowercase and slugify
         slug = Utils.slugify(subject)
-
         dir = "/subjects/#{slug}/"
         site.pages << SubjectsPage.new(site, site.source, dir, subject)
       end
